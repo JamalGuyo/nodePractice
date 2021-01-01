66dc050b940e2c64381df1bd96431f68264b7f41
Jamal Guyo Abgudo <jamalguyo08@gmail.com>
2021-01-01 20:44:31 +0300

    define custom error class

--- 2.errors/index.js
+++ 2.errors/index.js
@@ -1,8 +1,21 @@
 const express = require('express'),
 app = express();
+const AppError = require('./AppError');
 // routes
 app.get('/', (req, res) => {
     res.send('home page')
+})
+app.get('/error', (req,res) => {
+   try{
+       chicken.fly();
+   } catch(e){
+       throw new AppError('Chicken do not fly', 404)
+   }
+})
+// error handling middleware
+app.use((err,req,res,next) => {
+    const {status = 500, message ="something went wrong"} = err;
+    res.status(status).send(message);
 })
 // create listener
 const PORT = process.env.PORT || 8080;
