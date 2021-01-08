989e8539716df2c4e27b5f4521b593032cd594d0
Jamal Guyo <jamalguyo08@gmail.com>
2021-01-08 12:27:48 +0300

    create a session count variable that counts number of page visit

---
+++ 4.sessions/index.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+    app = express(),
+    session = require('express-session');
+// 
+app.use(session({
+    secret:'thisismysecret'
+}));
+// routes
+app.get('/', (req, res) => {
+    res.send('go to /viewcount');
+})
+app.get('/viewcount', (req,res) => {
+    if(req.session.count){
+        req.session.count += 1;
+    }else{
+        req.session.count = 1;
+    }
+    res.send(`You have visited this site ${req.session.count} times`)
+})
+// listener
+app.listen(8080, () => console.log('session app served on port 8080'));
