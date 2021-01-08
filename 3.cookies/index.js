082bff92ac84819bac2d16add532b7b38799d041
Jamal Guyo <jamalguyo08@gmail.com>
2021-01-08 11:44:51 +0300

    send and receive the signed cookies

--- 3.cookies/index.js
+++ 3.cookies/index.js
@@ -2,9 +2,10 @@ const express = require('express'),
     app = express(),
     cookieParser = require('cookie-parser');
 // 
-app.use(cookieParser());
+app.use(cookieParser('thisismysecret'));
 
 // routes
+// unsigned cookies
 app.get('/', (req, res) => {
     const {name= 'Jane Doe'} = req.cookies;
     res.send(`hello there ${name}`)
@@ -13,6 +14,15 @@ app.get('/', (req, res) => {
 app.get('/setname', (req, res) => {
     res.cookie('name', 'John');
     res.send('sent you a cookie')
+})
+// signed cookies
+// create signed cookies
+app.get('/signcookie', (req, res) => {
+    res.cookie('fruit', 'grape', {signed: true});
+    res.send('signed your cookie');
+})
+app.get('/showsign', (req, res) => {
+    res.send(req.signedCookies)
 })
 // listener
 const PORT = process.env.PORT || 8080;
