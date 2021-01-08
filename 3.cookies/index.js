e3767de7b86346613eb145e5f58a16e0f4a6e8f2
Jamal Guyo <jamalguyo08@gmail.com>
2021-01-08 11:40:30 +0300

    install cookie-parser and return unsigned cookies

--- 3.cookies/index.js
+++ 3.cookies/index.js
@@ -1,9 +1,13 @@
 const express = require('express'),
-    app = express();
+    app = express(),
+    cookieParser = require('cookie-parser');
+// 
+app.use(cookieParser());
 
 // routes
 app.get('/', (req, res) => {
-    res.send('cookie root page')
+    const {name= 'Jane Doe'} = req.cookies;
+    res.send(`hello there ${name}`)
 }) 
 
 app.get('/setname', (req, res) => {
