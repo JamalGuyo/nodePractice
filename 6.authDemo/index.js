64af2182b8c45bbc650d730df3decef917a9551d
Jamal Guyo <jamalguyo08@gmail.com>
2021-01-11 14:45:26 +0300

    create user registeration"

--- 6.authDemo/index.js
+++ 6.authDemo/index.js
@@ -1,13 +1,40 @@
 const express = require('express'),
 app = express(),
 path = require('path'),
-ejs = require('ejs');
+bcrypt = require('bcrypt'),
+mongoose = require('mongoose');
+// 
+const User = require('./models/user');
+// db
+mongoose.connect('mongodb://localhost:27017/authdemo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+.then(() => console.log('connected to authdemo db'))
+.catch(e => console.log(e));
 // configs
 app.set('view engine', 'ejs');
-app.set('view', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true}))
 // routes
+app.get('/', (req, res) => {
+    res.send('home page')
+})
 app.get('/register', (req, res) => {
-    res.send('register route');
+    res.render('register');
+})
+
+app.post('/register', async(req, res) => {
+    const {username, password} = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({
+        username, 
+        password: hashedPassword
+    })
+    await user.save();
+    res.redirect('/');
 })
 
 app.get('/secret', (req, res) => {
