5b96cde91c3179315a8f6c2e8ebd1522ec2cb6fd
Jamal Guyo <jamalguyo08@gmail.com>
2021-01-12 10:00:08 +0300

    move hashing logic to userschema static method

--- 6.authDemo/models/user.js
+++ 6.authDemo/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose'),
-    {Schema} = mongoose;
+    {Schema} = mongoose,
+    bcrypt = require('bcrypt');
 // schema
 const userSchema = new Schema({
     username: {
@@ -11,6 +12,16 @@ const userSchema = new Schema({
         required: [true, "password cannot be blank"]
     }
 })
+userSchema.statics.findAndValidate = async function(username, password){
+    const foundUser = await this.findOne({username});
+    const isValid = await bcrypt.compare(password, foundUser.password);
+    return isValid ? foundUser : false;
+}
 
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+})
 // export model
 module.exports = mongoose.model('User', userSchema);
