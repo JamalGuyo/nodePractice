8205b9f21555164982f08e16352b158abdb217f6
Jamal Guyo <jamalguyo08@gmail.com>
2021-01-12 11:44:06 +0300

    configure passport

--- 7.passportAuth/index.js
+++ 7.passportAuth/index.js
@@ -3,7 +3,13 @@ const express = require('express'),
     path = require('path'),
     mongoose = require('mongoose'),
     // ejsmate
-    ejsMate = require('ejs-mate');
+    ejsMate = require('ejs-mate'),
+    // auth
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
+    session = require('express-session'),
+    // model
+    User = require('./models/user');
 // connect to db
 mongoose.connect('mongodb://localhost:27017/passportdemo', {
     useNewUrlParser: true,
@@ -17,6 +23,18 @@ mongoose.connect('mongodb://localhost:27017/passportdemo', {
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+// auth configs
+app.use(session({
+    secret: 'thisisnotagoodsecret',
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 // routes
 app.get('/', (req, res) => {
     res.render('index');
